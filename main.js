f850c89aa9155421a79c84cecefdd8c4a5d91377
jscotty <jscotty@hotmail.nl>
2019-08-11 23:19:25 +0200

    Responsive canvas

--- main.js
+++ main.js
@@ -1,13 +1,16 @@
 import SceneManager from './scenes/sceneManager.js';
 import Menu from './scenes/menu.js';
 import Game from './scenes/game.js';
+import MathHelper from './helpers/mathhelper.js';
 import { sceneStates, uiAssets, gameAssets, artifactsAssets } from './helpers/references.js';
 
+const width = 800;
+const height = 600;
+
 const renderer= new PIXI.Application({
-    width: 800, height: 600, backgroundColor: 0x6cd6e0, resolution: window.devicePixelRatio || 1,
+    width: width, height: height, backgroundColor: 0x6cd6e0, resolution: window.devicePixelRatio || 1,
 });
 document.body.appendChild(renderer.view);
-
 
 PIXI.Loader.shared
 .add(uiAssets.mediumPinkButtons.sheet)
@@ -25,3 +28,14 @@ PIXI.Loader.shared
     });
 });
 
+var resize = () => {
+    const scaleFactor = MathHelper.clamp(Math.min(window.innerWidth / width, window.innerHeight / height), 0, 1);
+    var newWidth = width * scaleFactor;
+    var newHeight = height * scaleFactor;
+      
+    renderer.view.style.width = newWidth + 'px';
+    renderer.view.style.height = newHeight + 'px';
+};
+    
+window.addEventListener('resize', resize, false);
+resize();
